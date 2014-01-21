62799bec89d3750500ac79996d5d0696c12b934a
kenanuzel <kenan.uzel@windowslive.com>
2014-01-21 19:24:03 +0200

    Added timer for request timeout

--- eps/procedures/mme/s6a.py
+++ eps/procedures/mme/s6a.py
@@ -5,20 +5,33 @@ class AuthenticationInformationRetrievalProcedureHandler(object):
 
     Success, Failure = range(2)
 
-    def __init__(self, hssAddress, ioService, procedureCompletionCallback):
+    def __init__(self, hssAddress, ioService, responseTimeoutT3, procedureCompletionCallback):
         self.hssAddress = hssAddress
         self.ioService = ioService
         self.procedureCompletionCallback = procedureCompletionCallback
         self.nextEndToEndId = 0
         self.outstandingRequests = {}
+        self.responseTimeoutT3 = responseTimeoutT3
+        self.waitForResponseTimer = {}
+        self.timeoutCounter = 0
 
     def execute(self, imsi, visitedPlmnId):
-        self.ioService.sendMessage(self.hssAddress, *authenticationInformationRequest(imsi, visitedPlmnId, self.nextEndToEndId))
-        self.outstandingRequests[self.nextEndToEndId] = {"imsi": imsi, "visitedPlmnId": visitedPlmnId}
+        self.waitForResponseTimer[self.nextEndToEndId] = self.ioService.createTimer(self.responseTimeoutT3,
+                                                                           self.__onResponseTimeout__, self.nextEndToEndId)
+        self.waitForResponseTimer[self.nextEndToEndId].start()
+        self.ioService.sendMessage(self.hssAddress, *authenticationInformationRequest(imsi, visitedPlmnId, 
+                                                                                      self.nextEndToEndId))
+        self.outstandingRequests[self.nextEndToEndId] = {"imsi": imsi, "visitedPlmnId": visitedPlmnId}        
         self.nextEndToEndId += 1
+        
+    def __onResponseTimeout__(self, endToEndId):
+            self.procedureCompletionCallback(self.Failure)
+            del self.outstandingRequests[endToEndId]
+            self.timeoutCounter += 1
 
     def handleIncomingMessage(self, source, interface, channelInfo, message):
         endToEndId = channelInfo["endToEndId"]
+        self.waitForResponseTimer[endToEndId].cancel()
         if endToEndId in self.outstandingRequests:
             authContext = self.outstandingRequests[endToEndId]
             if message["resultCode"] == 2001:
@@ -27,3 +40,4 @@ class AuthenticationInformationRetrievalProcedureHandler(object):
             else:
                 self.procedureCompletionCallback(self.Failure)
             del self.outstandingRequests[endToEndId]
+        
